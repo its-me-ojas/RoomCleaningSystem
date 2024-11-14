4a3df70b744ecf4097d48a644ac012e5db9ff105
its-me-ojas <crestfallenwizard@gmail.com>
2024-11-14 15:53:52 +0530

    added starter files

---
+++ src/main/java/com/example/roomcleaningsystem/RoomCleaningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomcleaningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCleaningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomCleaningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roomcleaningsystem/RoomCleaningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomcleaningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleaningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
